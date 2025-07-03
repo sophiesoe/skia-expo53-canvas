7a05a9b65e5945edf5627bc6a31544e926f97126
sophiesoe <devsophiesoe@gmail.com>
2025-07-03 10:21:39 +0630

    init commit

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { View, Button } from "react-native";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import { Canvas, Path } from "@shopify/react-native-skia";
+import { runOnJS } from "react-native-reanimated";
 
-export default function App() {
+export default function Draw() {
+  const [paths, setPaths] = useState([]);
+
+  const startPath = (x, y) => {
+    setPaths((prev) => [
+      ...prev,
+      {
+        segments: [`M ${x} ${y}`],
+        color: "#06d6a0",
+      },
+    ]);
+  };
+
+  const updatePath = (x, y) => {
+    setPaths((prev) => {
+      const newPaths = [...prev];
+      console.log("paths", newPaths?.[0].segments);
+      const index = newPaths.length - 1;
+      if (newPaths[index]) {
+        newPaths[index].segments.push(`L ${x} ${y}`);
+      }
+      return newPaths;
+    });
+  };
+
+  const clearPaths = () => {
+    setPaths([]);
+  };
+
+  const pan = Gesture.Pan()
+    .onStart((g) => {
+      runOnJS(startPath)(g.x, g.y);
+    })
+    .onUpdate((g) => {
+      runOnJS(updatePath)(g.x, g.y);
+    })
+    .minDistance(1);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureDetector gesture={pan}>
+        <View style={{ flex: 1, backgroundColor: "black" }}>
+          <Canvas style={{ flex: 1 }}>
+            {paths.map((p, index) => (
+              <Path
+                key={index}
+                path={p.segments.join(" ")}
+                strokeWidth={10}
+                style="stroke"
+                color={p.color}
+                strokeCap="round"
+              />
+            ))}
+          </Canvas>
+          <View style={{ position: "absolute", top: 50, right: 20 }}>
+            <Button title="Reset" onPress={clearPaths} color="#ef476f" />
+          </View>
+        </View>
+      </GestureDetector>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
